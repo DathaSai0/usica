22218ee6f3f28311d555826ef0e95f933259c972
Dathasai0 <lakkojuleeladathasai@gmail.com>
2025-10-16 12:38:03 +0530

    initial commit

---
+++ src/main/java/com/usica/usica/UsicaApplication.java
@@ -0,0 +1,13 @@
+package com.usica.usica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsicaApplication.class, args);
+	}
+
+}
